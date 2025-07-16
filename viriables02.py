db848e501beb9541375f82ef0d76a981bea61e25
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-16 15:37:18 +0800

    Add: data types

--- viriables02.py
+++ viriables02.py
@@ -5,3 +5,16 @@
 this is a block comment
 
 '''
+
+
+# Variables
+n= 10
+n= "drazen"
+print(n) #Comments can be placed after the code too
+
+# Data types
+
+name = "Harvey"#string - str (An array of characters)
+age = 10 # Integer - int (A whole number)
+height = 1.50 # Float - float ( It is a number with a decimal point)
+isKid = True # Boolean - bool (True or False)
