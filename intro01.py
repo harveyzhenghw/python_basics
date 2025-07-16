1651c518ba23ce9f819b12d05030e899338f7a21
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-16 15:11:26 +0800

    Initial commit

---
+++ intro01.py
@@ -0,0 +1,13 @@
+print("Hi, I am Python :D")
+print("And what is your name? ")
+
+name = input()
+
+
+print(f"Hello {name}, nice to meet you!")
+
+print("I was  born in 1991, thanks to a programmer called Guido van Rossum.")
+
+year_of_birth = input("What year were you born? ")
+age = 2025 - int(year_of_birth)
+print(f"Wow, you are {age} years old! That's great!")
