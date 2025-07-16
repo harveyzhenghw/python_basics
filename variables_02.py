1e97474cac718e7b030758a0647d9ba2bc5bcebb
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-16 15:57:57 +0800

    Add: variable name explanation

--- variables_02.py
+++ variables_02.py
@@ -18,3 +18,10 @@ name = "Harvey"#string - str (An array of characters)
 age = 10 # Integer - int (A whole number)
 height = 1.50 # Float - float ( It is a number with a decimal point)
 is_kid = True # Boolean - bool (True or False)
+# variable names matter
+
+number = 3# This one is ok
+Number = 5#This one do not use in python
+NUMBER = 10# This one is a constant ( you give this name when the variable shouldn't be changed)
+PI = 3.14# contant
+print(number,Number,NUMBER,PI)
