68277d418f097359db2c3ae0eb713255bf0ed6a1
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-16 20:36:23 +0800

    Add: homework

---
+++ homework_03.py
@@ -0,0 +1,28 @@
+# Basic data types:
+#int
+a = 10
+print(a, type(a))
+a = -2560
+print(a, type(a))
+
+# float
+b = 125.1238
+print(b, type(b))
+b = -0.9991
+print(b,type(b))
+
+#bool
+c= True
+print(c,type(c))
+c= False
+print(c, type(c))
+
+#str
+d = " Your Text "
+print(d, type(d))
+d = ' This is a text too '
+print(d, type(d))
+d = "23"
+print(d,type(d))
+d = "a"
+print(d, type(d))
