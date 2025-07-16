c0bf5c56c8bd8bc132cf03d0aa4da0b56570eb28
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-16 15:44:15 +0800

    Fix: naming convention

--- variables_02.py
+++ variables_02.py
@@ -17,4 +17,4 @@ print(n) #Comments can be placed after the code too
 name = "Harvey"#string - str (An array of characters)
 age = 10 # Integer - int (A whole number)
 height = 1.50 # Float - float ( It is a number with a decimal point)
-isKid = True # Boolean - bool (True or False)
+is_kid = True # Boolean - bool (True or False)
