7081e7d0c42f02aad267a61d18214697a434ea34
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-24 15:30:45 +0800

    Code: relational and logical oparators

--- rel&lob_task.py
+++ rel&lob_task.py
@@ -1,3 +1,6 @@
 print("welcome to the roller coaster theme park")
 height = int(input("enter your height: "))
-print(height >= 150)
+print(height >= 150 and height <=200)
+
+
+
--- relational_and_logical_a.py
+++ relational_and_logical_a.py
@@ -10,3 +10,20 @@ print(5 < 15000) # True # ==, >, >=, <=
 print(120 != 120) # False # ==, >=, !=, <=
 print(60 < 15) # False # ==, <, !=, >=
 print(25.3421 == 25.3421) # True # ==, <, !=, <=
+
+has_money = False
+is_high_enough = True
+
+print(is_high_enough and has_money)
+
+# OR => means at least one need to be true
+
+has_money = False
+dad_will_give_money = False
+
+print(has_money or dad_will_give_money)
+
+# not is flipping the value
+
+print(not has_money)
+
