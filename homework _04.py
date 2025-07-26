0b5e3879116433e5e09b7564a167b3cc884fb299
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-07-26 11:15:50 +0800

    Code: homework math ops

---
+++ homework _04.py
@@ -0,0 +1,48 @@
+'''Homework:
+Make sure you can answer the questions below:
+
+How can we gather data from the program user?
+How can we get an integer value from user?input
+What are arithmetic operators we know?+-*/
+ %modulo
+ >grater than
+ < less then
+ == equal
+ != not equal
+ <= less then or equal
+ >= grater then or equal
+
+What is the type of return value of division?remainders
+What is a modulo operation? What is integral division?a mudulo is a thing that gets remainders
+How are logical values interpreted in mathematical operations?
+How does addition and multiplication work on strings? What can we add and
+multiply?if inside the sting is a number it is converted into a number if not then it is a error
+8. How does fstring work? if there is a fstring it makes it into a sentence
+9. What kind of built-in functions did we explore and what are their purposes?
+
+& modulo remeinders
+> grater then
+< less then
+== equal
+!= not equal
+<= less then or equal
+>= grater then or equal
+'''
+
+
+
+
+
+
+
+
+
+'''Exercise
+Insert and, or, not into each line to get expected result'''
+print(True, 25 < 140 and 10 == 10)
+print(True, 100 >= 1 or 2 > 10)
+print(False, 25 < 14 and 10 != 10)
+print(False, -1 < 3 and 2 < 9 and 10 == 15)
+print(True, 20.05 < 21 < 10 or -10 < 20 < 150 <= 150)
+print(False, 1 < 10 and 2 < 15 and -50 == 42)
+print(True, not 2 == 10)
